fe0430274b1f35affeeb4934e85455ea028ca9cc
GitHub Action <action@github.com>
2022-01-19 10:38:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/clemtar/oddevenbackgroundmethods/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.clemtar.oddevenbackgroundmethods.services
+
+import com.github.clemtar.oddevenbackgroundmethods.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
